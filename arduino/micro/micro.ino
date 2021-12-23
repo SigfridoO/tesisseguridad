78db0b4adc2e9bba9d83f6dc4730593d989d0df6
Sigfrido <sigfrido_o@ardilluda.com.mx>
2021-12-22 21:26:37 -0600

    Se agrega el archivo micro con el codigo anterior

---
+++ arduino/micro/micro.ino
@@ -0,0 +1,73 @@
+///////////////////Codigo para declarar pines de entrada y salida
+
+int di_0=5;
+int di_1=6;
+int di_2=7;
+int di_3=8;
+int di_4=9;
+int di_5=10;
+
+int do_0=11;
+int do_1=12;
+int do_2=13;
+//[]
+byte X[6];
+byte Y[3];
+//SOLO UNA EJECUCIÓN
+void setup() {
+//Lectura de entradas
+
+  pinMode(di_0,INPUT);
+  pinMode(di_1,INPUT);
+  pinMode(di_2,INPUT);
+  pinMode(di_3,INPUT);
+  pinMode(di_4,INPUT);
+  pinMode(di_5,INPUT);
+
+  pinMode(do_0, OUTPUT);
+  pinMode(do_1, OUTPUT);
+  pinMode(do_2, OUTPUT);
+
+
+
+   /////////////////////Codigo para inicializar puertos de comunicación
+   Serial.begin(9600);
+
+}
+
+void loop() {
+ for(int i=0;i<6;i++){
+Serial.print(X[i]);
+if(i==5){
+Serial.println();
+}
+Serial.write(" ,");
+ }
+
+
+ /////////////////////Codigo para control del programa
+
+Y[0]=0;
+
+
+actualizarPines();
+
+}
+
+
+void actualizarPines(){
+
+X[0]= digitalRead (di_0);
+X[1]= digitalRead (di_1);
+X[2]= digitalRead (di_2);
+X[3]= digitalRead (di_3);
+X[4]= digitalRead (di_4);
+X[5]= digitalRead (di_5);
+
+digitalWrite (do_0,Y[0]);
+digitalWrite (do_1,Y[1]);
+digitalWrite (do_2,Y[2]);
+
+
+Serial.print ("\t" );
+  }
