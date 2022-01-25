e601750f06f921c24ae7f3846d7666c7f0483aa3
David <drv171298@hotmail.com>
2022-01-24 20:40:01 -0600

    Activación por medio de comandos codificados

--- arduino/micro/micro.ino
+++ arduino/micro/micro.ino
@@ -1,29 +1,30 @@
-///////////////////Codigo de comunicacion
+///////////////////Codigo de comunicacion////////////////////////////
 #define TAMANIO_BUFFER 100
 byte bufferLectura[TAMANIO_BUFFER];
 
-///////////////////Codigo de comunicacion////////////////////////////
-
 int indiceBuffer = 0;
 
-char caracterInicio = '{';
-char caracterFinal = '}';
+char caracterInicio = '#';
+char caracterFinal = '&';
+
+//////////////////////////////Funciones de usuario//////////////
 
 void leerAlmacenarSerie ();
 void decodificarInstrucion();
 
+int senialGPS0();
+int senialGPS1();
+int activManual();
+int desactivManual();
 
-String intruccion1 = "juan";
 String extraerInstruccion(byte*, int, int);
-
+//String identificarInstruccion(byte*, int, int);
 
 void actualizarPines();
-//char* enviarAlarmas();
 void apagarAlarma();
 char caracterLeido();
+
 ///////////////////Codigo para declarar pines de entrada y salida//////
-
-
 int di_0=5;
 int di_1=6;
 int di_2=7;
@@ -40,8 +41,6 @@ byte Y[8];
 byte M[48];
 
 
-
-//SOLO UNA EJECUCIÓN
 void setup() {
   // Definir pines de entrada y salida
 
@@ -57,11 +56,10 @@ void setup() {
   pinMode(do_2, OUTPUT);
 
 
-
-   /////////////////////Codigo para inicializar puertos de comunicación
+/////////////////////Codigo para inicializar puertos de comunicación
    Serial.begin(9600);
 //M[1]=Identificación de entrada 1 --> Respuesta (%=Abierto y *Cerrado)
-M[2]=0; // 0=Usuario fuera, 1=Usuario dentro --> Señal GPS ()
+M[2]=1; // 0=Usuario fuera, 1=Usuario dentro --> Señal GPS ()
 //M[3]=0; // 1=Encendido módo manual, 0= Apagado modo manual --> Señal de encenddido manual --> Petisión #_%1A24&
 //I0=Interruptor electromágnetico del zaguan --> *
 //M4=Encendido del sistema --> Fisico #_%1A24&
@@ -73,68 +71,89 @@ M[2]=0; // 0=Usuario fuera, 1=Usuario dentro --> Señal GPS ()
 //M[10]=0; // Activación por alarma programada por horario, 1=Encendido y 0=Apagado
 //Q[0]=Activación de cerradura electromagnetica
 //M[11]=Botón de activación manual
-//M[12]=Botón de desactivación manual
-
+M[12]=0; //Botón de desactivación manual
 }
 
 
-
-
 void loop() {
-    
-
      
-    /////////////////////Codigo para comunicación
+/////////////////////Codigo para comunicación/////////////////////////
     leerAlmacenarSerie();
     decodificarInstrucion();
     
-    /////////////////////Codigo para control del programa
+/////////////////////Codigo para control del programa////////////////
     
-    M[3]=X[1];
-    M[7]=X[2];
-    M[1]=X[0] & (!M[2] | M[3]);
-    M[4]=(M[1] | M[4]) & !M[2] & !M[3];
+    //M[3]=0;
+    //M[7]=X[2];
+    M[1]=(X[0] & !M[2] ) | (X[0] & M[3]);
+    M[4]=((M[1] | M[4]) & !M[2]) | ((M[1] | M[4]) & M[3]);
     M[5]= M[4] & M[1];
     M[6]=!M[5] & !M[1];
     M[8]=M[6] & !M[7];
     //M[10]=M[9] &
-    Y[0]= !M[2] | M[3];
-    M[3]= (M[11] | M[3] | M[10]) & M[12];
+    Y[0]=M[4] & (!M[2] | M[3]);
+    M[3]= (M[11] | (M[3] && !M[10]) | M[10]) & !M[12];
+
+/////////////////Mensajes de información del sistema/////////////
+      
+    /*Serial.print(" X[0]= ");
+    Serial.print(X[0]);
+    Serial.println();*/
 
 
-    
-    //if(M[4]==1){
-    //Y[0]=M[8];
-    //}
-    
-    /*
-    M[4]=(M[1] | M4[4]) & !M[2] & !M[3];
-    //Contador
-    if(i>=1){
-    M[6]= !M[5] & !M[1];
-    }
-    M[8]=M[6] & !M[7];
-    //M[10]= TOF1.Q
-    Q[0]= !M[2] | M[3];
-    M[3]= (M[11] | M[3] | M[10]) & !M[12];*/
-    
-    
-//    Serial.print(" X[0]= ");
-//    Serial.print(X[0]);
-//    Serial.print(" M[1]= ");
-//    Serial.print(M[1]);
-//    Serial.print(" M[4]= ");
-//    Serial.print(M[4]);
-//    Serial.print(" M[6]= ");
-//    Serial.print(M[6]);
-//    Serial.println();
-    
+if(M[1] == 1 && M[4] == 1){
+    Serial.print("Zaguan Cerrado"); // #1A15&  
+    Serial.println();    
+}
 
+if(M[1] == 0 && M[4] == 1){
+    Serial.print("Zaguan Abierto"); // #0A15&
+    Serial.println();
+}
 
-    //////////// Actualización de las entradas y salidas
+if(M[4] == 1){
+    Serial.print("Sistema Activado"); 
+    Serial.println();
+}
+
+if(M[5] == 1 && M[4] == 1){
+    Serial.print("Lugar Asegurado"); // #1A054& 
+    Serial.println();
+}
+
+if(M[6] == 1 && M[4] == 1){
+    Serial.print("Sistema violentado"); // #1A064&
+    Serial.println();
+}
+
+if(M[8] == 1 && M[4] == 1){
+    Serial.print("Llamada en curso"); // #1A084& 
+    Serial.println();  
+}
+
+if(Y[0] == 1 && M[4] == 1){
+    Serial.print("Cerradura Zaguan activada"); // #1Y014&
+    Serial.println();
+}
+if(M[3] == 1 && M[11] == 1){
+    Serial.print("Activado manualmente"); // #1B114& 
+    Serial.println();  
+}
+
+if(M[3] == 0 && M[12] == 1){
+    Serial.print("desactivado manualmente");  // #1B124&
+    Serial.println();  
+}
+
+if(M[3] == 1 && M[10] == 1){
+    Serial.print("Acivación por horario"); // #1B104&
+    Serial.println();  
+}
+
+    
+//////////// Actualización de las entradas y salidas////////////////
     actualizarPines();
 
-    
 }
 
 
@@ -154,8 +173,6 @@ void actualizarPines(){
     //Serial.print ("\t" );
 }
 
-
-
 char* enviarAlarmas(){
     
     char Mensaje[64];
@@ -165,7 +182,7 @@ char* enviarAlarmas(){
 
 void apagarAlarma (){
 
-  M[3]=1;
+  //M[3]=1;
 
 }
 
@@ -197,7 +214,6 @@ void decodificarInstrucion() {
   Serial.println();
 */
 
-
   char instrucion = 0;
   boolean inicioDeInstruccionEncontrado = false;
   int posicionInicio= 0;
@@ -205,7 +221,6 @@ void decodificarInstrucion() {
   byte * cadena;
   String instruccionDetectada = "";
 
-  
   for (int i = 0; i < indiceBuffer; i++) {
 
     if (bufferLectura[i] == caracterInicio ) {
@@ -259,28 +274,50 @@ String extraerInstruccion(byte* bufferLectura, int posicionInicio, int posicionF
     //Serial.write(bufferLectura [i]);
     inst += (char) bufferLectura [i];
   }
+  
   //Serial.println();
-  //Serial.print (inst);
-  return inst;
-  
-  
-}
+  Serial.print (inst);
 
-
-
-void funcion_1(){
-  
-;
-}
-
-
-void funcion_2(){
-
-  for (int i = 0; i < indiceBuffer; i++) {
-    // buscar caracter de inicio
-   Serial.print(":::::::::::::::::::::::::::");
-    
+  if(inst == "#0B024&"){
+    senialGPS0();
+    //M[2]=0;
+    Serial.print("Usuario Fuera");
+    Serial.println();
   }
 
-  
+  if (inst == "#1B024&"){
+    senialGPS1();
+    //M[2]=1;
+    Serial.print("Usuario dentro");
+    Serial.println();
+  }
+  if (inst == "#1B114&"){ 
+    activManual();  
+    Serial.print("Activación manual");
+    Serial.println();
+  }
+  if (inst == "#1B124&"){
+    desactivManual(); 
+    Serial.print("Desactivación manual");
+    Serial.println();
+  }  
+  delay(2000);
+
+ //return inst;
+}
+
+int senialGPS0(){
+  M[2]=0;
+}
+
+int senialGPS1(){
+  M[2]=1;
+}
+
+int activManual(){
+  M[11]=1;
+}
+
+int desactivManual(){
+  M[12]=1;
 }
